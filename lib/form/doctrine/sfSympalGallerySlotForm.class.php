b082ab3520e32dd5656c1fad3ff16e5ea81f7879
phiamo <phiamo@gmail.com>
2010-06-27 05:28:40 +0200

    Removed SlotRelation completely

--- lib/form/doctrine/sfSympalGallerySlotForm.class.php
+++ lib/form/doctrine/sfSympalGallerySlotForm.class.php
@@ -7,9 +7,11 @@
  * @author     Philipp A. Mohrenweiser <phiamo@gmail.com>
  */
 class sfSympalGallerySlotForm extends sfSympalContentSlotForm{
+  /**
+   * add gallery choosign an parameterizing form
+   */
   public function configure()
   {
-
     parent::configure();  	#var_dump($this->widgetSchema);
 
     $this->widgetSchema['galleries_list'] = new sfWidgetFormDoctrineChoice(array('model'=>'sfSympalGallery'));
@@ -37,42 +39,31 @@ class sfSympalGallerySlotForm extends sfSympalContentSlotForm{
   	return $gForm;
   }
   /**
-   * Do not save embedded form!
+   * Do not save embedded form! its just used to get parameters here
    * @see lib/vendor/symfony/lib/form/addon/sfFormObject#updateObjectEmbeddedForms($values, $forms)
    */
   public function saveEmbeddedForms($values = null, $forms = null){
-  	
   }
-  public function updateObjectEmbeddedForms($values, $forms = null){
-  	parent::updateObjectEmbeddedForms($values, $forms);
-  }
+  /**
+   * get the InlineGallery from current value
+   */
   protected function getInlineObjectGalleryFromValue(){
   	require_once(dirname(__FILE__).'/../../../../../lib/vendor/symfony/lib/helper/HelperHelper.php');
     use_helper('InlineObject');
     $parser = get_inline_object_parser();
     $objects = $parser->parseTypes($this->getObject()->getValue());
-    return $objects[1][0];
-    
+    return $objects[1][0];  
   }
-  public function updateDefaultsFromObject()
-  {
-    parent::updateDefaultsFromObject();
-
-    if (isset($this->widgetSchema['galleries_list']))
-    {
-      $this->setDefault('galeries_list', $this->object->Galleries->getPrimaryKeys());
-    }
-
-  }
-  public function doSave($con = null)
-  {
-    $this->saveGalleriesList($con);
-
-    parent::doSave($con);
-  }
+  /**
+   * insert inline object syntax to valus
+   */
   public function updateValueColumn(){
   	return $this->getGalleryInline();
   }
+  /**
+   * generate syntax
+   *  (isnt there a function in inline object to generate ?)
+   */
   public function getGalleryInline(){
   	$inline = $this->getInlineObjectGalleryFromValue();
   	$galleries = $this->getValue('galleries_list');
@@ -84,41 +75,5 @@ class sfSympalGallerySlotForm extends sfSympalContentSlotForm{
   	}
   	return '[gallery:'.$galleries[0].$params.']';
   }
-  public function saveGalleriesList($con = null)
-  {
-    if (!$this->isValid())
-    {
-      throw $this->getErrorSchema();
-    }
-
-    if (!isset($this->widgetSchema['galleries_list']))
-    {
-      // somebody has unset this widget
-      return;
-    }
-
-    if (null === $con)
-    {
-      $con = $this->getConnection();
-    }
-
-    $existing = $this->object->Galleries->getPrimaryKeys();
-    $values = $this->getValue('galleries_list');
-    if (!is_array($values))
-    {
-      $values = array();
-    }
-
-    $unlink = array_diff($existing, $values);
-    if (count($unlink))
-    {
-      $this->object->unlink('Galleries', array_values($unlink));
-    }
-
-    $link = array_diff($values, $existing);
-    if (count($link))
-    {
-      $this->object->link('Galleries', array_values($link));
-    }
   }
 }
