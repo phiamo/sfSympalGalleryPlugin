8647527167f5d2963b0a98a17a78694a86a97e5d
phiamo <phiamo@gmail.com>
2010-06-27 04:54:02 +0200

    Merged GalleryAsset in

---
+++ lib/form/doctrine/PluginsfSympalGalleryAssetForm.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * PluginsfSympalGalleryAsset form.
+ *
+ * @package    sfSympalGallery
+ * @subpackage form
+ * @author     Philipp A. Mohrenweiser <phiamo@gmail.com>
+ */
+abstract class PluginsfSympalGalleryAssetForm extends BasesfSympalGalleryAssetForm
+{
+}
